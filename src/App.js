47f429fe287d6c59038d992fed47fd14a3df72b8
Alan Osta <alanosta@gmail.com>
2022-05-16 14:36:00 -0300

    MVP - Requerimientos basicos implementados

--- src/App.js
+++ src/App.js
@@ -22,16 +22,17 @@ const ALL_COUNTRIES_CODE = gql`
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [groupByButton, setGroupByButton] = useState("continent");
+  
   const {data, loading, error} = useQuery(ALL_COUNTRIES_CODE)  
   if (error) return <span>{error}</span>
 
   return (    
     <div className="App">
-        <h1>Country search</h1>
-        <input type="text" name="country" onChange={e => setInputValue(e.target.value)} value={inputValue}/>
-        <h2>Group by: (not implemented)</h2> {/* No implementado */}
-        <button onClick={() => setGroupByButton("continent")}>Continent</button> {/* No implementado */}
-        <button onClick={() => setGroupByButton("languages")}>Language</button> {/* No implementado */}
+        <h2>Country search</h2>
+        <input type="text" name="country" onChange={e => setInputValue(e.target.value)} value={inputValue} placeholder="Type in some text to begin searching..."/>
+        <h2>Group by:</h2> 
+        <button onClick={() => setGroupByButton("continent")} className={groupByButton === "continent" ? "active" : ""}>Continent</button>
+        <button onClick={() => setGroupByButton("languages")} className={groupByButton === "languages" ? "active" : ""}>Language</button>
         {loading ? <p>Loading...</p> : <PrintCountries countries={data.countries} textFilter={inputValue} groupByValue={groupByButton}/>}      
     </div>
   )
--- src/components/PrintCountries/PrintCountries.jsx
+++ src/components/PrintCountries/PrintCountries.jsx
@@ -1,23 +1,25 @@
 import React from "react"
 
-export const PrintCountries = ({countries, textFilter, groupByValue}) => {
+export const PrintCountries = ({countries, textFilter, groupByValue="continen"}) => {
     
     // Validaciones iniciales
     if (countries === null || !textFilter) return null
-    
-    //console.log(groupByValue) 
-    
+    //if (countries === null ) return null
+        
     // Filtrado de paises por input text
     let filteredCountries = countries.filter(country => country.name.toLowerCase().includes(textFilter.toLowerCase()));
     
-    // lista unica de continetes en el array de paises buscados
+    // lista de continentes unicos en el array de paises buscados
     let uniqueContinents = [...new Set(filteredCountries.map(country => country.continent.name))]
 
-    // lista unica de idiomas en el array de paises buscados
-    // ... pendiente
+    // lista de idiomas unicos en el array de paises buscados
+    let uniqueLanguages = new Set();
+    filteredCountries.forEach(country => country.languages.forEach( language => uniqueLanguages.add(language.name) ))
+    let uniqueLanguagesList = Array.from(uniqueLanguages)
 
-    return (
-        <>
+    if (groupByValue === "continent") {
+        return (
+            <>
             {uniqueContinents.map(continent =>                
                 <div key={continent}>
                     <h2 key={continent}>{continent}</h2>
@@ -27,12 +29,42 @@ export const PrintCountries = ({countries, textFilter, groupByValue}) => {
                         <div key={country.code} className="card">
                             <h4>{country.emoji} {country.name}</h4><br />
                             <div>Continent: {country.continent.name}</div>
+                            <div>Languages:
+                                <ul key={country.code+"_langs"}> 
+                                    {country.languages.map(language => <li key={language.name+"_langs"}>{language.name}</li>)}
+                                </ul>
+                            </div>
                         </div> 
                         : null 
                     )}
                     </div>
                 </div>
             )}     
-        </>
-    )
+            </>
+        )
+    } else {
+        return (
+            <>  
+            {uniqueLanguagesList.map(language =>                
+                <div key={language}>
+                    <h2 key={"h2_"+language}>{language}</h2>
+                    <div className="cardGroup">
+                        {filteredCountries.map(country => country.languages.find(lang => lang.name === language) ?
+                        <div key={country.code} className="card">
+                            <h4>{country.emoji} {country.name}</h4><br />
+                            <div>Continent: {country.continent.name}</div>
+                            <div>Languages:
+                                <ul key={country.code+"_langs"}> 
+                                    {country.languages.map(language => <li key={language.name+"_langs"}>{language.name}</li>)}
+                                </ul>
+                            </div>
+                        </div> 
+                        : null
+                    )}
+                    </div>
+                </div>
+            )}     
+            </>
+        )
+    }
 }
